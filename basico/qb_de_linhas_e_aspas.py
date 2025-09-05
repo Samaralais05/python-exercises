f941270492ddcb33d1880b1a8c2e0a2634385907
Samara Laís <samara.lais@outlook.com.br>
2025-09-05 14:19:35 +0200

    Adicionar quebra de linhas com \n e aspas

---
+++ basico/qb_de_linhas_e_aspas.py
@@ -0,0 +1,19 @@
+#Desafio: Escreva um código de uma linha usando a
+#função de print(), bem como os caracteres de nova linha e de escape,
+#para corresponder ao resultado esperado gerado em três linhas.
+
+#Saída esperada
+
+#"Eu sou"
+#""aprendizado""
+#"""Python"""
+
+print("\"Eu sou\"\n\"\"aprendizado\"\"\n\"\"\"Python\"\"\"")
+
+#abrindo o trabalho com = ("
+#para as " do Eu sou = \"Eu sou\"
+#para a quebra de linha = \n
+#para duplas aspas de aprendizado = \"\"aprendizado\"\"
+#para quebra de linha = \n
+#para 3 aspas de python = \"\"\"Python\"\"\"
+#fechando o trabalho com = ")
