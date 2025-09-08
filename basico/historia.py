0fb4acb1a55ed3b8571909f8d6c5a65e71858331
Samara Laís <samara.lais@outlook.com.br>
2025-09-08 13:54:59 +0200

    adicionar teorema de pitagoras e historia

---
+++ basico/historia.py
@@ -0,0 +1,7 @@
+john=3
+mary=5
+adam=6
+total_apples=john+mary+adam
+print(adam, mary, john)
+print(total_apples)
+
---
+++ basico/teorema_de_pitagoras.py
@@ -0,0 +1,9 @@
+#Agora você deve ser capaz de construir um programa curto
+#que resolva problemas matemáticos simples, como o teorema de Pitágoras:
+
+#O quadrado da hipotenusa é igual à soma dos quadrados dos outros dois lados.
+
+a=3.4
+b=4.0
+c=(a ** 2 + b ** 2) ** 0.5
+print("c =", c)
